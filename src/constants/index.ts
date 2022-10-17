365f4e96ed0b7e9ac1130d787ac151164753bd2f
clinton-slice <ccnkemdilim@gmail.com>
2022-10-16 23:41:50 -0400

    feat: add constants to project

---
+++ src/constants/index.ts
@@ -0,0 +1,4 @@
+export const OPEN_CHARGE_API_URL = "https://api.openchargemap.io/v3/poi";
+export const OPEN_CHARGE_API_KEY = "123456";
+export const EV_ENERGY_CHARGE = "https://example.ev.energy/chargingsession";
+export const MAX_RESULTS = "50";
